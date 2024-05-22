36f2fd513e51ad1907d98cdf30cc452ff05e32f1
choffman_code <choffman.toast@gmail.com>
2024-05-22 11:55:22 -0500

    card styling fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {CourseList} from './components/CourseList'
-import {Banner} from './components/Banner'
+import {CourseList} from './components/CourseList';
+import {Banner} from './components/Banner';
+import './App.css';
 
 
-console.log("HERE2")
+
 const schedule = {
   "title": "CS Courses for 2018-2019",
   "courses": {
@@ -31,8 +32,7 @@ const schedule = {
 };
 
 const App = () =>  (
-  <div>
-    <p>test</p>
+  <div className="container">
     <Banner title={ schedule.title } />
     <CourseList courses={ schedule.courses } />
   </div>
--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -9,7 +9,12 @@ const getCourseNumber = course => (
 );
 
 export const Course = ({ course }) => (
-  <div>
-    { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
+  <div className="card m-1 p-2">
+    <div className="card-body">
+      <h5 className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</h5>
+      <div className="card-text">{ course.title }</div>
+    </div>
+    <div><hr/></div>
+    <div className="text-center">{ course.meets }</div>
   </div>
 );
