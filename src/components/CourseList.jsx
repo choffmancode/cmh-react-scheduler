02879acc16c91801daaa95b1094a281760ee25bf
choffman_code <choffman.toast@gmail.com>
2024-05-27 14:25:27 -0500

    added course & term filtering

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -3,14 +3,19 @@ import TermSelector from './TermSelector';
 import React, { useState } from 'react';
 import { getCourseTerm } from './Course';
 
+export const toggle = (x, lst) => (
+  lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst]
+);
+
 export const CourseList = ({ courses }) => {
   const [term, setTerm] = useState('Fall');
+  const [selected, setSelected] = useState([]);
   const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
   return (
     <>
       <TermSelector term={term} setTerm={setTerm} />
       <div className="course-list">
-      { termCourses.map(course => <Course key={course.id} course={ course } />) }
+      { termCourses.map(course => <Course key={course.term + course.number} course={ course } selected={selected} setSelected={ setSelected } />) }
       </div>
     </>
   );
